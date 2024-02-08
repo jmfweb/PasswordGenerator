112679e680a4e80628e0b4ceecb102c3df9fda92
jmfweb <fernandezjuanj1@gmail.com>
2024-02-08 17:47:26 +0100

    First stable version

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -9,5 +9,31 @@ import { Component, OnInit } from "@angular/core";
 export class GeneratorComponent implements OnInit {
 	constructor() {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		function generatePassword(): string {
+			const length: number = Math.floor(Math.random() * (12 - 8 + 1)) + 8; // Generates a random length between 8 and 12 characters
+			const characters: string =
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}[]|\\;:'\",.<>?";
+			let password: string = "";
+			for (let i: number = 0; i < length; i++) {
+				const randomChar: string = characters.charAt(Math.floor(Math.random() * characters.length));
+				password += randomChar;
+			}
+			return password;
+		}
+
+		function showPassword() {
+			const generatedPassword: string = generatePassword();
+			const passwordInput: HTMLInputElement = document.getElementById(
+				"password"
+			) as HTMLInputElement;
+			passwordInput.value = generatedPassword;
+		}
+
+		// Get the button and add a listener for the click event
+		const generateButton: HTMLButtonElement = document.getElementById(
+			"generateButton"
+		) as HTMLButtonElement;
+		generateButton.addEventListener("click", showPassword);
+	}
 }
