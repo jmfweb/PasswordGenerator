f3453f3077f824afcae2e5a16b16468b9b96675f
jmfweb <fernandezjuanj1@gmail.com>
2024-02-08 16:16:14 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { GeneratorComponent } from "./generator/generator.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, GeneratorComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'PasswordGenerator';
+	title = "PasswordGenerator";
 }
---
+++ src/app/generator/generator.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-generator",
+	standalone: true,
+	templateUrl: "./generator.component.html",
+	styleUrls: ["./generator.component.css"],
+})
+export class GeneratorComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+}
